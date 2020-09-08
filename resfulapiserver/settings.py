4b9b65676a0d369d513d18638763d2afdb1470ca
park-mk <whezme@naver.com>
2020-09-08 21:51:37 +0900

    change

--- resfulapiserver/settings.py
+++ resfulapiserver/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 AUTH_USER_MODEL = 'Users.Users'
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','192.168.43.184','192.168.43.225']
 
 
 # Application definition
